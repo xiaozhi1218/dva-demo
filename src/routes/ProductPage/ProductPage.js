a5941fb154f7f9b57a90a97f9bc491c69bf41a05
chenyongzhi <chenyongzhijjk@163.com>
2021-04-05 15:09:06 +0800

    init

---
+++ src/routes/ProductPage/ProductPage.js
@@ -0,0 +1,27 @@
+import React from "react"
+import Product from "../../components/Product"
+import { connect } from "dva"
+
+class IndexPage extends React.Component{
+    render(){
+        const { productList,dispatch } = this.props;
+        return(
+            <div>
+                <Product dispatch={ dispatch } title="商品类型" productList={ productList } />
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return{
+        productList:state.product
+    }
+}
+
+export default connect(mapStateToProps)(IndexPage);
+
+// 解构赋值写法
+// export default connect(({ product }) => ({
+//     product,
+//   }))(IndexPage);
